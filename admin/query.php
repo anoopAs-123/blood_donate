7fd7f2f52910f515b0d166fa73a4fc7ddefa6b9a
anoopAs-123 <142108950+anoopAs-123@users.noreply.github.com>
2024-05-16 11:59:54 -0700

    Update query.php

--- admin/query.php
+++ admin/query.php
@@ -66,8 +66,8 @@ include 'conn.php';
             document.getElementById("demo").innerHTML = "Read";
             <?php echo '<div class="alert alert-info alert_dismissible"><b><button type="button" class="close" data-dismiss="alert">&times;</button></b><b>Pending Request "Read".</b></div>';
             $que_id = $_GET['id'];
-            $sql1="update contact_query set query_status='1' where  query_id={$que_id}";
-            // $result=mysqli_query($conn,$sql1);
+            $sql1="update contact_query set query_status='1' where  query_id='$que_id'";
+            $result=mysqli_query($conn,$sql1);
             ?>
         }
       }
